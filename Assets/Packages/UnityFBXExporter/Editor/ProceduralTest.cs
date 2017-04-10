ecd18af932ef7c9813430afe35ea3405c667315e
Kellan Higgins <kellan@8bitgoose.com>
2017-04-10 12:12:04 -0700

    Add procedural test to ensure mikelortegas code works

---
+++ Assets/Packages/UnityFBXExporter/Editor/ProceduralTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class ProceduralTest : MonoBehaviour 
+{
+
+	[MenuItem("Assets/FBX Exporter/Create Object With Procedural Texture", false, 43)]
+	public static void CreateObject()
+	{
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		Texture2D texture = new Texture2D(128, 128);
+		for (int x = 0; x < 128; ++x)
+			for (int y = 0; y < 128; ++y)
+				texture.SetPixel(x, y, (x-64)*(x-64) + (y-64)*(y-64) < 1000 ? Color.white : Color.black);
+		texture.Apply();
+		Material mat = new Material(Shader.Find("Standard"));
+		mat.mainTexture = texture;
+		cube.GetComponent<MeshRenderer>().sharedMaterial = mat;
+	}
+}
